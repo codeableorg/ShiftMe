c85020ec2d5d3731dbcea29cfed3da7ed83dd984
Angie Cristina <angiecristinagonzalessara@gmail.com>
2019-07-17 17:15:39 -0500

    Add routes and User Controller

--- Api/config/routes.rb
+++ Api/config/routes.rb
@@ -3,7 +3,18 @@ Rails.application.routes.draw do
   # sessions routes
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-    get 'schedules/index'
+
+    resources :users, only: [:show, :index] 
+
+    scope 'schedule' do
+      resources :workshifts, only: :index
+      # get 'schedule/workshifts' # lista de workshifts#index
+      resources :requests, except: :destroy
+      # post 'schedule/requests' # ca show request#show
+      # patch 'schedule/requests/:id'mbio de workshifts request#create
+      # get 'schedule/requests' # lista requests#index
+      # get 'schedule/requests/:id' # # actualizar request#update
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
