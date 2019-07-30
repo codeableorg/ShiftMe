a18969393d3768dee31acba150b4fa9d36028470
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-07-30 12:10:07 -0500

    Add status validations for request model

--- Api/app/models/request.rb
+++ Api/app/models/request.rb
@@ -1,5 +1,19 @@
 # app/models/request.rb
 class Request < ApplicationRecord
+  ROLES = {
+    manager: 'admin',
+    user: 'FrontDesk'
+  }.freeze
+
+  STATUS = {
+    pending: 'Pending',
+    agree: 'Agree',
+    disagree: 'Disagree',
+    accepted: 'Accepted',
+    rejected: 'Rejected',
+    cancel: 'Cancel'
+  }.freeze
+
   has_many :request_notification, class_name: 'Notification', foreign_key: 'request_id'
   belongs_to :requester, class_name: 'User'
   belongs_to :requested, class_name: 'User'
@@ -9,22 +23,9 @@ class Request < ApplicationRecord
   after_create :create_notification
   scope :manager, -> { where(rol: ROLES[:manager], status: STATUS[:pending]) }
 
+  validates :status, inclusion: { in: STATUS, message: "%{value} is not a valid status" }
   validate :validate_requester
   after_update :notify_admin
-
-  ROLES = {
-    manager: 'admin',
-    user: 'FrontDesk'
-  }.freeze
-
-  STATUS = {
-    pending: "Pending",
-    agree: "Agree",
-    disagree: "Disagree",
-    accepted: "Accepted",
-    rejected: "Rejected",
-    cancel: "Cancel"
-  }.freeze
 
   def validate_requester
     errors.add(:requester, "Can't be request") if requester == requested
