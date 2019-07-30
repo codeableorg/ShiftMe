b25db5af626e3e184de989512cb28801f87de302
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-07-30 14:50:59 -0500

    Add update workshift method to search an specific workshift and change the shift_id

--- Api/app/models/schedule.rb
+++ Api/app/models/schedule.rb
@@ -1,3 +1,10 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  def update_workshift(current_shift, requested_shift)
+    requested_schedule.workShifts.map! do |work_shift|
+      work_shift[:shift_id] = requested_shift if work_shift[:shift_id] == current_shift
+    end
+    requested_schedule.save!
+  end
 end
--- Api/app/models/user.rb
+++ Api/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :requester_request, class_name: 'Request', foreign_key: 'requester_id'
   has_many :requested_request, class_name: 'Request', foreign_key: 'requested_id'
   has_many :notify_user, class_name: 'Notification', foreign_key: 'notify_user_id'
+  has_many :schedules
   has_secure_password
   has_secure_token
 
