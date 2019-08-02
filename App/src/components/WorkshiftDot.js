33cc2ed9ab50b959f4864f45564c576c1d263336
Sergio Xalambrí <hello@sergiodxa.com>
2019-08-02 12:48:54 -0500

    Create colored dot for workshifts

--- App/src/components/WorkshiftDot.js
+++ App/src/components/WorkshiftDot.js
@@ -1,7 +1,11 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 
+<<<<<<< HEAD
 export function WorkshiftDot({ title, shiftId, size = "medium" }) {
+=======
+export function WorkshiftDot({ title, shiftId }) {
+>>>>>>> Create colored dot for workshifts
   const backgroundColor =
     shiftId === 1
       ? "#F7C948"
@@ -9,9 +13,13 @@ export function WorkshiftDot({ title, shiftId, size = "medium" }) {
       ? "#EF4E4E"
       : shiftId === 3
       ? "#3EBD93"
+<<<<<<< HEAD
       : shiftId === 4
       ? "#7B8794"
       : "#BED0F7";
+=======
+      : "#7B8794";
+>>>>>>> Create colored dot for workshifts
   return (
     <figure
       title={title}
@@ -19,8 +27,13 @@ export function WorkshiftDot({ title, shiftId, size = "medium" }) {
         margin: 0,
         display: "inline-block",
         borderRadius: "50%",
+<<<<<<< HEAD
         width: size === "medium" ? 15 : 10,
         height: size === "medium" ? 15 : 10,
+=======
+        width: "15px",
+        height: "15px",
+>>>>>>> Create colored dot for workshifts
         backgroundColor
       }}
     />
